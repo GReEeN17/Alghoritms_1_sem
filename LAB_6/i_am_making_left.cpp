ceca363cc67b0fb92d8285e901a259537ea6054a
Aleksandr <azelen04@mail.ru>
2022-12-09 23:34:46 +0300

    Made plot for making left rotation

---
+++ LAB_6/i_am_making_left.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void leftLittle (int mas[][3], int n) {
+    
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int mas[n][3];
+    for (int i = 0; i < n; i++) {
+        cin >> mas[i][0] >> mas[i][1] >> mas[i][2];
+    }
+
+}

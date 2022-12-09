22e51c3c6aac8bd5f4ce9861e203613ed1489fc3
Aleksandr <azelen04@mail.ru>
2022-12-09 13:11:05 +0300

    Made check_balance

---
+++ LAB_6/balance_check.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void balance(int mas[][3], int height[], int n) {
+    for (int i = n - 1; i >= 0; i--) {
+        height[i + 1] = 1 + max(height[mas[i][1]], height[mas[i][2]]);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int mas[n][3];
+    for (int i = 0; i < n; i++) {
+        cin >> mas[i][0] >> mas[i][1] >> mas[i][2];
+    }
+    int height[n];
+    balance(mas, height, n);
+    for (int i = 0; i < n; i++) {
+        cout << height[mas[i][2]] - height[mas[i][1]] << "\n";
+    }
+}

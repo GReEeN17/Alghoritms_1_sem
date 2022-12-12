d093cfed09d74fb239ec485d5035681cef473620
Aleksandr <azelen04@mail.ru>
2022-12-12 23:19:22 +0300

    Made freeing telephone

--- LAB_6/freeing_telphone.cpp
+++ LAB_6/freeing_telphone.cpp
@@ -1,123 +1,127 @@
 #include <iostream>
 using namespace std;
+int res = 0;
 
 struct nodeFT {
     string val;
     unsigned char height;
-    nodeFT* left;
-    nodeFT* right;
-    nodeFT(string v) { val = v; left = right = nullptr; height = 1; }
+    nodeFT* left = nullptr;
+    nodeFT* right = nullptr;
 };
 
+unsigned char heightFT(nodeFT* p)
+{
+    return p?p->height:0;
+}
 
-struct listFT {
-    nodeFT* root = nullptr;
-    int res = 0;
+int bfactorFT(nodeFT* p)
+{
+    return heightFT(p->right)-heightFT(p->left);
+}
 
-    unsigned char height(nodeFT* p) {
-        return p ? p->height : 0;
+void fixheightFT(nodeFT* p)
+{
+    unsigned char hl = heightFT(p->left);
+    unsigned char hr = heightFT(p->right);
+    p->height = (hl>hr?hl:hr)+1;
+}
+
+nodeFT* rotaterightFT(nodeFT* p)
+{
+    nodeFT* q = p->left;
+    p->left = q->right;
+    q->right = p;
+    fixheightFT(p);
+    fixheightFT(q);
+    return q;
+}
+
+nodeFT* rotateleftFT(nodeFT* q)
+{
+    nodeFT* p = q->right;
+    q->right = p->left;
+    p->left = q;
+    fixheightFT(q);
+    fixheightFT(p);
+    return p;
+}
+
+nodeFT* balanceFT(nodeFT* p)
+{
+    fixheightFT(p);
+    if( bfactorFT(p)==2 )
+    {
+        if( bfactorFT(p->right) < 0 )
+            p->right = rotaterightFT(p->right);
+        return rotateleftFT(p);
     }
-
-    int bFactor(nodeFT* p) {
-        return height(p->right) - height(p->left);
+    if( bfactorFT(p)==-2 )
+    {
+        if( bfactorFT(p->left) > 0  )
+            p->left = rotateleftFT(p->left);
+        return rotaterightFT(p);
     }
+    return p;
+}
 
-    void fixHeight(nodeFT* p) {
-        unsigned char hl = height(p->left);
-        unsigned char hr = height(p->right);
-        p->height = (hl > hr ? hl : hr) + 1;
+nodeFT* insertFT(nodeFT* p, string k)
+{
+    if( !p ) {
+        nodeFT* gap_node = new nodeFT;
+        gap_node->val = k;
+        gap_node->height = 1;
+        return gap_node;
     }
+    if( k<p->val)
+        p->left = insertFT(p->left,k);
+    else
+        p->right = insertFT(p->right,k);
+    return balanceFT(p);
+}
 
-    nodeFT* rotateRight(nodeFT* p) {
-        nodeFT* q = p->left;
-        q->left = p->right;
-        q->right = p;
-        fixHeight(p);
-        fixHeight(q);
-        return q;
+nodeFT* exists(string val, nodeFT* gap_root) {
+    if (gap_root == nullptr || gap_root->val == val) {
+        return gap_root;
+    } else if (gap_root->val > val) {
+        return exists(val, gap_root->left);
+    } else {
+        return exists(val, gap_root->right);
     }
+}
 
-    nodeFT* rotateLeft(nodeFT* q) {
-        nodeFT* p = q->right;
-        q->right = p->left;
-        p->left = q;
-        fixHeight(q);
-        fixHeight(p);
-        return p;
-    }
-
-    nodeFT* balance(nodeFT* p) {
-        fixHeight(p);
-        if (bFactor(p) == 2) {
-            if (bFactor(p->right) < 0) {
-                p->right = rotateRight(p->right);
-            }
-            return rotateLeft(p);
-        }
-        if (bFactor(p) == -2) {
-            if (bFactor(p->left) > 0) {
-                p->left = rotateLeft(p->left);
-            }
-            return rotateRight(p);
-        }
-        return p;
-    }
-
-    nodeFT* insert(nodeFT* p, string k) {
-        if (!p) return new nodeFT(k);
-        if (k < p->val) {
-            p->left = insert(p->left, k);
-        } else {
-            p->right = insert(p->right, k);
-        }
-        return balance(p);
-    }
-
-    nodeFT* exists(string val, nodeFT* gap_root) {
-        if (gap_root == nullptr || gap_root->val == val) {
-            return gap_root;
-        } else if (gap_root->val > val) {
-            return exists(val, gap_root->left);
-        } else {
-            return exists(val, gap_root->right);
-        }
-    }
-
-    nodeFT* insertCheck (nodeFT* q, string k) {
-        nodeFT* newNode = exists(k, root);
-        if (newNode != nullptr) {
-            int count = 0;
-            bool flag;
-            for (int i = 0; i < k.length(); i++) {
-                flag = true;
-                for (int j = i + 1; j < k.length(); j++) {
-                    if (k[i] == k[j]) {
-                        flag = false;
-                        break;
-                    }
+nodeFT* checkInsertTFC (nodeFT* gap_root, string k) {
+    nodeFT* newNode = exists(k, gap_root);
+    if (newNode != nullptr) {
+        int count = 0;
+        bool flag;
+        for (int i = 0; i < k.length(); i++) {
+            flag = true;
+            for (int j = i + 1; j < k.length(); j++) {
+                if (k[i] == k[j]) {
+                    flag = false;
+                    break;
                 }
-                if (flag) {
-                    count += 1;
-                }
             }
-            res += count;
-            return root;
-        } else {
-            return insert(root, k);
+            if (flag) {
+                count += 1;
+            }
         }
+        res += count;
+        return gap_root;
+    } else {
+        return insertFT(gap_root, k);
     }
-};
+}
 
-
-int freeing_tele() {
-    listFT li;
+int freeing_telephone() {
     int n;
     cin >> n;
     string temp;
+    nodeFT* root = nullptr;
     for (int i = 0; i < n; i++) {
         cin >> temp;
-        li.root = li.insertCheck(li.root, temp);
+        root = checkInsertTFC(root, temp);
     }
-    cout << li.res;
+    cout << res;
     return 0;
 }

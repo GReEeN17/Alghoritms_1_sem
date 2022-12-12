e17d9003b13367c1c2612fcec8cc6156bc32795c
Aleksandr <azelen04@mail.ru>
2022-12-13 02:13:56 +0300

    Made first variant of feeding cats

---
+++ LAB_6/feeding_cats.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+void buildTreeFC (int tree[], int mas[], int v, int L, int R, int n) {
+    if (L == R - 1) {
+        if (L < n) {
+            tree[v] = mas[L];
+        }
+        return;
+    }
+    int M = (L + R) / 2;
+    buildTreeFC(tree, mas, 2 * v + 1, L, M, n);
+    buildTreeFC(tree, mas, 2 * v + 2, M, R, n);
+    tree[v] = tree[2 * v + 1] + tree[2 * v + 2];
+}
+
+int getSumFC (int tree[], int v, int L, int R, int l, int r) {
+    if (r <= L || R <= l) return 0;
+    if (l <= L && R <= r) return tree[v];
+    int M = (L + R) / 2;
+    int first_child = getSumFC(tree, 2 * v + 1, L, M, l, r);
+    int second_child = getSumFC(tree, 2 * v + 2, M, R, l, r);
+    return first_child + second_child;
+}
+
+void updateFC(int tree[], int mas[], int v, int L, int R, int i, int x, char func) {
+    if (L == R - 1)
+    {
+        if (func == '+') {
+            tree[v] += x;
+            mas[i] += x;
+        } else {
+            tree[v] -= x;
+            mas[i] -= x;
+        }
+        return;
+    }
+    int M = (L + R) / 2;
+    if (i < M) updateFC(tree, mas, 2 * v + 1, L, M, i, x, func);
+    else updateFC(tree, mas, 2 * v + 2, M, R, i, x, func);
+    tree[v] = tree[2 * v + 1] + tree[2 * v + 2];
+}
+
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    char func;
+    int digit_1, digit_2;
+    int mas[n];
+    int tree[n * 10];
+    for (int i = 0; i < n * 10; i++) {
+        if (i < n) {
+            mas[i] = 0;
+        }
+        tree[i] = 0;
+    }
+    buildTreeFC(tree, mas, 0, 0, n * 10, n);
+    for (int i = 0; i < q; i++) {
+        cin >> func >> digit_1 >> digit_2;
+        if (func == '?') {
+            cout << getSumFC(tree, 0, 0, n * 10, digit_1 - 1, digit_2) << "\n";
+        } else {
+            updateFC(tree, mas, 0, 0, n * 10, digit_1 - 1, digit_2, func);
+        }
+    }
+    return 0;
+}

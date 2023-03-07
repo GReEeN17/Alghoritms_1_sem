1b620b213629cf9a8defd86763c6e9a866c12063
Aleksandr <azelen04@mail.ru>
2023-03-08 00:22:11 +0300

    Made lab 9_2

--- LAB-9/city_of_bridges.cpp
+++ LAB-9/city_of_bridges.cpp
@@ -4,45 +4,48 @@ using namespace std;
 
 class Graph {
 private:
-    vector<int> *arr_paths;
-    bool *visited;
+    vector<vector<int>>arr_paths;
+    vector<int> visited;
     int vertexes;
 public:
+    bool cycle_found = false;
 
     Graph(int v);
 
     void add_edge(int dep, int dest);
 
-    void dfs(int vertex);
+    void dfs(int vertex, int start, int len);
 
     void reset_visited();
 };
 
 Graph::Graph(int v) {
     vertexes = v;
-    arr_paths = new vector<int>[vertexes];
-    visited = new bool[vertexes];
+    arr_paths = vector<vector<int>>(vertexes);
+    visited = vector<int>(vertexes);
 }
 
 void Graph::add_edge(int dep, int dest) {
     arr_paths[dep].push_back(dest);
 }
 
-void Graph::dfs(int vertex) {
-    visited[vertex] = true;
+void Graph::dfs(int vertex, int prev, int start) {
+    visited[vertex] = 1;
     vector<int> arr_path = arr_paths[vertex];
-    cout << vertex << "\n";
     vector<int>::iterator i;
     for (i = arr_path.begin(); i != arr_path.end(); i++) {
-        cout << visited[*i] << " " << *i << "\n";
-        if (!visited[*i]) dfs(*i);
+        if (visited[*i] == 0) {
+            dfs(*i, vertex, start);
+        } else if (visited[*i] == 1 && *i != prev) {
+            cycle_found = true;
+            break;
+        }
     }
+    visited[vertex] = 2;
 }
 
 void Graph::reset_visited() {
-    for (int i = 0; i < vertexes; i++) {
-        visited[i] = false;
-    }
+    visited = {0};
 }
 
 int main() {
@@ -52,15 +55,17 @@ int main() {
     int temp_1, temp_2;
     for (int i = 0; i < m; i++) {
         cin >> temp_1 >> temp_2;
-        g.add_edge(temp_1, temp_2);
-        g.add_edge(temp_2, temp_1);
+        g.add_edge(temp_1 - 1, temp_2 - 1);
+        g.add_edge(temp_2 - 1, temp_1 - 1);
     }
-    for (int i = 1; i <= n; i++) {
-        g.dfs(i);
+    for (int i = 0; i < n; i++) {
+        g.dfs(i, -1, i);
+        if (g.cycle_found) {
+            cout << "YES" << "\n";
+            return 0;
+        }
         g.reset_visited();
-        cout << "------------------" << "\n";
     }
+    cout << "NO" << "\n";
+    return 0;
 }
-//When i graduated from school I was totally disillusioned, that i won't see my classmates every day. I was frusatrated
-//with this fact, but I understood that on that my life doesn't stop. And i was totally right. When I enrolled in university I
-// felt gleeful that I met so amny new friends. Also I was petrified when

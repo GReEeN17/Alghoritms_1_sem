e7d5d9696ebb2d465678b4bcb5cddd3609023b3f
Aleksandr <azelen04@mail.ru>
2023-05-10 17:51:05 +0300

    Made lab_12

--- LAB_12/CocksMoscow.cpp
+++ LAB_12/CocksMoscow.cpp
@@ -1,101 +1,77 @@
 #include <iostream>
 #include <vector>
-#include <set>
 using namespace std;
 
 class CocksMoscow {
 private:
     int height;
     int width;
-    int size;
-    int leftFraction;
-    int rightFraction;
-    vector<vector<int>> graph;
-    vector<int> mt;
-    vector<int> used;
-    set<int> crossedCellsRight;
-    set<int> crossedCellsLeft;
+    vector<vector<bool>> field;
+    vector<vector<bool>> used;
+    vector<vector<pair<int, int>>> match;
 
-    bool kuhn(int v) {
-        if (used[v] != 0) return false;
-        used[v] = 1;
-        for (int i = 0; i < graph[v].size(); i++) {
-            int to = graph[v][i];
-            bool ifContinue = false;
-            for (int j : crossedCellsRight) {
-                if (j == to) {
-                    ifContinue = true;
-                    break;
-                }
-            }
-            if (ifContinue) continue;
-            if (mt[to] == -1 || kuhn(mt[to])) {
-                mt[to] = v;
+    bool dfs(int first, int second) {
+        if (used[first][second]) return false;
+        used[first][second] = true;
+        vector<pair<int, int>> neigh;
+        if (first > 0 && field[first - 1][second]) {
+            neigh.push_back({first - 1, second});
+        }
+        if (first < height - 1 && field[first + 1][second]) {
+            neigh.push_back({first + 1, second});
+        }
+        if (second > 0 && field[first][second - 1]) {
+            neigh.push_back({first, second - 1});
+        }
+        if (second < width - 1 && field[first][second + 1]) {
+            neigh.push_back({first, second + 1});
+        }
+        for (auto iter : neigh) {
+            int f = iter.first;
+            int s = iter.second;
+            if (match[f][s] == pair<int, int>{-1, -1} || dfs(match[f][s].first, match[f][s].second)) {
+                match[f][s] = {first, second};
                 return true;
             }
         }
         return false;
     }
 public:
-    CocksMoscow (int height, int width) {
-        this->height = height;
-        this->width = width;
-        this->size = height * width;
-        this->leftFraction = size % 2 == 0 ? size / 2 : size / 2 + 1;
-        this->rightFraction = size / 2;
-        graph.resize(leftFraction);
-        mt.assign(rightFraction, -1);
-        used.assign(leftFraction, 0);
-        for (int i = 0; i < size; i += 2) {
-            if (i >= width) {
-                graph[i / 2].push_back((i - width) / 2);
-            }
-            if (i < size - width) {
-                graph[i / 2].push_back((i + width) / 2);
-            }
-            if (i % width > 0) {
-                graph[i / 2].push_back((i - 1) / 2);
-            }
-            if (i % width < width - 1) {
-                graph[i / 2].push_back((i + 1) / 2);
-            }
-        }
+    CocksMoscow(int N, int M) {
+        height = N;
+        width = M;
+        field.resize(N, vector<bool>(M, true));
+        used.resize(N, vector<bool>(M, false));
+        match.resize(N, vector<pair<int, int>>(M, {-1, -1}));
     }
 
-    void crossCells() {
-        for (int i : crossedCellsLeft) {
-            used[i] = 2;
-        }
+    void crossCell(int h, int w) {
+        field[h][w] = false;
     }
 
-    void addCrossedCell(int w, int h) {
-        if ((w * (h - 1) + w) % 2 == 0) {
-            crossedCellsRight.insert((w * (h - 1) + w - 1) / 2);
-        } else {
-            crossedCellsLeft.insert((w * (h - 1) + w - 1) / 2);
-        }
-    }
-
-    int countCells() {
+    int kuhn() {
         int res = 0;
-        for (int i = 0; i < leftFraction; i++) {
-            used.assign(leftFraction, 0);
-            crossCells();
-            if (kuhn(i)) res += 2;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (field[i][j]) {
+                    used.assign(height, vector<bool>(width, false));
+                    if (dfs(i, j)) res++;
+                }
+            }
         }
         return res;
     }
 };
 
-int cocksMoscowMain() {
+int main() {
     int N, M, Q;
     cin >> N >> M >> Q;
-    CocksMoscow cocksMoscow(N, M);
+    CocksMoscow matching(N, M);
     for (int i = 0; i < Q; i++) {
-        int w, h;
-        cin >> w >> h;
-        cocksMoscow.addCrossedCell(w, h);
+        int height, width;
+        cin >> height >> width;
+        matching.crossCell(height - 1, width - 1);
     }
-    cout << cocksMoscow.countCells();
+    cout << matching.kuhn();
     return 0;
 }

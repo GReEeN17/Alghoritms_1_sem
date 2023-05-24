b23bff248bc04ba77adecd26bf1f9e636e084619
Aleksandr <azelen04@mail.ru>
2023-05-24 08:21:26 +0300

    Made lab_13

--- LAB_13/RoadsidePicnic.cpp
+++ LAB_13/RoadsidePicnic.cpp
@@ -1,41 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
-#define capacity 100001
+#define capacity 1892790
+#define k 42
 
-int hashFuncRoadside(int key) {
-    return key % capacity;
+int hashFuncRoadside(int key, int i) {
+    int temp = floor(key * pow(k, i));
+    return temp % capacity;
 }
 
-int roadsidePicnic() {
+int main() {
     int N;
     cin >> N;
-    vector<bool> hashTable(capacity, false);
+    vector<int> hashTable(capacity, 0);
+    vector<int> first;
     for (int i = 0; i < N; i++) {
         int temp;
         cin >> temp;
-        hashTable[hashFuncRoadside(temp)] = true;
+        first.push_back(temp);
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            hashTable[hashFuncRoadside(first[j], i)] = first[j];
+        }
     }
     int M;
     cin >> M;
-    int res = 0, gap_res = 0;
-    bool isPrev = false;
+    int res = 0;
     for (int i = 0; i < M; i++) {
-        int temp;
+        int temp = 0;
         cin >> temp;
-        if (hashTable[hashFuncRoadside(temp)]) {
-            gap_res++;
-            isPrev = true;
-        } else {
-            if (isPrev) {
-                res = max(res, gap_res);
-            }
-            gap_res = 0;
-            isPrev = false;
+        if (hashTable[hashFuncRoadside(temp, i)] == temp) {
+            res++;
         }
-    }
-    if (isPrev) {
-        res = max(res, gap_res);
     }
     cout << res;
     return 0;
